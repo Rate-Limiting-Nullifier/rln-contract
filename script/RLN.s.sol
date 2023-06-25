c14e9d5a5425eaeb462de730af90923653bc7ba0
Magamedrasul Ibragimov <curryrasul@gmail.com>
2023-06-25 15:13:28 +0400

    feat: add deploy scripts

--- script/RLN.s.sol
+++ script/RLN.s.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
+import "../src/RLN.sol";
+import "../src/Verifier.sol";
 
 contract RLNScript is Script {
-    function setUp() public {}
-
     function run() public {
-        vm.broadcast();
+        uint256 minimalDeposit = vm.envUint("MINIMAL_DEPOSIT");
+        uint256 depth = vm.envUint("DEPTH");
+        uint8 feePercentage = uint8(vm.envUint("FEE_PERCENTAGE"));
+        address feeReceiver = vm.envAddress("FEE_RECEIVER");
+        uint256 freezePeriod = vm.envUint("FREEZE_PERIOD");
+        address token = vm.envAddress("ERC20TOKEN");
+
+        vm.startBroadcast();
+
+        Verifier verifier = new Verifier();
+        RLN rln = new RLN(minimalDeposit, depth, feePercentage, feeReceiver, freezePeriod, token, address(verifier));
+
+        vm.stopBroadcast();
     }
 }
